9d88ae3b370d589a0b65bd34026ee85f5bb8b5c4
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-10-19 06:05:15 +0530

    Add files via upload

---
+++ src/Client.cpp
@@ -0,0 +1,43 @@
+/*
+   Quest :: A-Hypertextual-Web-Search-Engine
+   Author:: Saugata Debnath
+*/
+
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include<ctype.h>
+#include <signal.h>
+#include<arpa/inet.h>
+#define SA struct sockaddr
+
+
+using namespace std;
+
+
+int main(int argc,char**argv){
+
+   struct sockaddr_in servaddr,cliaddr;
+   int sockfd=socket(AF_INET,SOCK_STREAM,0);
+   bzero(&servaddr,sizeof(servaddr));
+
+   servaddr.sin_family=AF_INET;
+   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+   servaddr.sin_port=htons(atoi(argv[1]));
+
+   inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+   connect(sockfd,(SA*)&servaddr,sizeof(servaddr));
+   
+   cout << "Hi, What are you wanna Quest for?  " ;
+   char search_text[1000];
+   scanf("%s",search_text);
+   write(sockfd,search_text,sizeof(search_text));
+
+   close(sockfd);
+
+}
---
+++ src/Server.cpp
@@ -0,0 +1,25 @@
+/*
+   Quest :: A-Hypertextual-Web-Search-Engine
+   Author:: Saugata Debnath
+*/
+
+#include <bits/stdc++.h>
+#include <fstream>
+#include <string>
+#include "LinkedList.h"
+#include "Page.h"
+#include "Read.h"
+#include "Rank.h"
+#include "Search.h"
+
+using namespace std;
+int main(int argc,char**argv){
+      
+
+	LinkedList* listPtr = new LinkedList();
+	const int numOfPages = 10;
+	read(numOfPages, listPtr);
+	ranking(numOfPages, listPtr);
+	listPtr->Sort();
+        search(numOfPages, listPtr,argv[1]);
+}
